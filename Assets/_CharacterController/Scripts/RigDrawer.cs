c5a1fc9488161fb67f9353602521ab4a854a6c56
Andrew Connell <Drew.L.Connell@gmail.com>
2020-09-14 15:58:04 -0400

    Initial Project

---
+++ Assets/_CharacterController/Scripts/RigDrawer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class RigDrawer : MonoBehaviour
+{
+    public GameObject origin = null;
+
+    private void OnDrawGizmos()
+    {
+        if (origin != null)
+        {
+            Gizmos.color = Color.green;
+            GizmoHelpers.DrawWireCubeOriented(origin.transform.position, origin.transform.rotation, 3.0f * origin.transform.localScale.x);
+        }
+    }
+}
---
+++ Assets/_CharacterController/Scripts/XRCharacter.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRCharacter : MonoBehaviour
+{
+    // Input values
+
+    // References
+
+    // Components
+
+    // Values
+
+    private void Awake()
+    {
+        // Collect our components
+    }
+
+    private void Update()
+    {
+        // If the controller is enabled, check for input
+    }
+
+    private void CheckForMovement(InputDevice device)
+    {
+        // Look for input, and potential value
+
+        // Sets character direction, also factoring head
+
+        // Apply character direction, and speed
+
+        // Orient the character mesh seperately
+
+        // Animate blend tree
+    }
+
+    private void CalculateDirection(Vector2 joystickDirection)
+    {
+        // Joystick direction
+
+        // Look rotate
+
+        // Rotate our joystick direction using the rotation of the head
+    }
+
+    private void MoveCharacter()
+    {
+        // Figure out how much we should move
+
+        // Use simple move to include gravity, frame independent by default
+    }
+
+    private void OrientMesh()
+    {
+        // Set the direction the character should look, only with input
+    }
+
+    private void Animate()
+    {
+        // Blend between walk/run using length, which is a value between 0 - 1
+    }
+}
